59aeebff31561a809f51cca3d905de438493846e
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-11 21:52:12 +0200

    projects page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Logo from "./components/Logo";
 import Navbar from "./components/Navbar";
 import profileImg from "./assets/developer-pic-1.png";
 import { motion } from "framer-motion";
+import projectImg from "./assets/fashion-studio-website.jpg";
+import { BsGithub } from "react-icons/bs";
+
 function App() {
   const textVariants = {
     initial: {
@@ -13,7 +16,7 @@ function App() {
       y: 0,
       opacity: 1,
       transition: {
-        duration: 1,
+        duration: 1.5,
         stagggerChildren: 0.1,
       },
     },
@@ -21,13 +24,15 @@ function App() {
 
   return (
     <main className="min-h-screen">
-      <header className="flex items-center justify-between w-full px-32 py-8 font-medium fixed top-0">
+      {/* {NAVBAR} */}
+      <header className="flex items-center justify-between w-full px-32 py-8 font-medium text-slate-200">
         <Navbar />
         <Logo />
         <Icons />
       </header>
 
-      <article className="flex items-center justify-center min-h-screen mt-[160px]">
+      {/* {HOMEPAGE} */}
+      <section className="flex items-center justify-center min-h-screen mt-[160px]">
         <div className="h-full w-full p-32 !pt-0 flex justify-between items-start">
           <div className="basis-1/2">
             <img src={profileImg} alt="Image background" />
@@ -62,7 +67,7 @@ function App() {
               animate="animate"
               className="flex gap-5 mt-2"
             >
-              <button className="basis-1/4 text-xl p-2 font-semibold capitalize text-slate-800 bg-slate-100 rounded-lg border-2 hover:border-slate-100 hover:bg-slate-900 hover:text-slate-200 shadow-[-2px_-2px_30px_2px_rgba(255,255,255,0.6),_20px_20px_30px_4px_rgba(45,78,255,0.15)]">
+              <button className="basis-1/4 text-xl p-2 font-semibold capitalize text-slate-800 bg-slate-200 rounded-lg border-2 hover:border-slate-100 hover:bg-slate-900 hover:text-slate-200 shadow-[-2px_-2px_30px_2px_rgba(255,255,255,0.5),_20px_20px_30px_4px_rgba(45,78,255,0.15)]">
                 Resume
               </button>
               <motion.button
@@ -80,7 +85,216 @@ function App() {
             </motion.div>
           </motion.div>
         </div>
-      </article>
+      </section>
+
+      {/* {PROJECTS} */}
+      <section className="p-32 w-full min-h-screen">
+        <div className="py-2 w-full mx-auto flex items-center justify-center">
+          <h1 className="text-8xl font-bold w-full capitalize mb-16 text-center text-slate-200 tracking-[1rem]">
+            Projects
+          </h1>
+        </div>
+        <div className="flex flex-col gap-y-20">
+          <div className="w-full flex-col items-center justify-center rounded-3xl rounded-br-2xl border border-solid border-l-cyan-900 shadow-[-2px_-2px_30px_2px_rgba(255,255,255,0.5),_20px_20px_30px_4px_rgba(45,78,255,0.15)] overflow-hidden">
+            <a
+              href="https://github.com/uyenlai"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer overflow-hidden rounded-lg"
+            >
+              <motion.img
+                src={projectImg}
+                alt="project 1"
+                className="h-auto w-full rounded-[2rem] p-5"
+                whileHover={{
+                  opacity: 0.9,
+                  scale: 1.01,
+                  transition: {
+                    duration: 0.5,
+                  },
+                }}
+              />
+            </a>
+
+            <div className="flex flex-col items-start justify-between p-8">
+              <h3 className="text-xl font-medium text-teal-500">
+                Featured Project
+              </h3>
+              <h2 className="my-2 w-full text-left text-4xl font-bold text-slate-200">
+                Project 1
+              </h2>
+              <p className="my-2 rounded-md font-medium text-slate-300">
+                A feature-rich Crypto Screener App using React, Tailwind CSS,
+                Context API, React Router and Recharts. It shows detail
+                regarding almost all the cryptocurrency. You can easily convert
+                the price in your local currency.
+              </p>
+              <div className="flex gap-4 items-center mt-2">
+                <motion.a
+                  href="https://github.com/uyenlai"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white"
+                  whileHover={{
+                    opacity: 0.8,
+                    scale: 1.2,
+                    transition: {
+                      duration: 0.3,
+                    },
+                  }}
+                >
+                  <BsGithub className="w-8 h-8" />
+                </motion.a>
+                <motion.a
+                  href="https://github.com/uyenlai"
+                  rel="noopener noreferrer"
+                  className="rounded-lg bg-slate-200 p-2 px-6 text-lg font-semibold text-slate-800"
+                  whileHover={{
+                    scale: 1.05,
+                    transition: {
+                      duration: 0.3,
+                    },
+                  }}
+                >
+                  Visit project
+                </motion.a>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex-col items-center justify-center rounded-3xl rounded-br-2xl border border-solid border-l-cyan-900 shadow-[-2px_-2px_30px_2px_rgba(255,255,255,0.5),_20px_20px_30px_4px_rgba(45,78,255,0.15)] overflow-hidden">
+            <a
+              href="https://github.com/uyenlai"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer overflow-hidden rounded-lg"
+            >
+              <motion.img
+                src={projectImg}
+                alt="project 1"
+                className="h-auto w-full rounded-[2rem] p-5"
+                whileHover={{
+                  opacity: 0.9,
+                  scale: 1.01,
+                  transition: {
+                    duration: 0.5,
+                  },
+                }}
+              />
+            </a>
+
+            <div className="flex flex-col items-start justify-between p-8">
+              <h3 className="text-xl font-medium text-teal-500">
+                Featured Project
+              </h3>
+              <h2 className="my-2 w-full text-left text-4xl font-bold text-slate-200">
+                Project 1
+              </h2>
+              <p className="my-2 rounded-md font-medium text-slate-300">
+                A feature-rich Crypto Screener App using React, Tailwind CSS,
+                Context API, React Router and Recharts. It shows detail
+                regarding almost all the cryptocurrency. You can easily convert
+                the price in your local currency.
+              </p>
+              <div className="flex gap-4 items-center mt-2">
+                <motion.a
+                  href="https://github.com/uyenlai"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white"
+                  whileHover={{
+                    opacity: 0.8,
+                    scale: 1.2,
+                    transition: {
+                      duration: 0.3,
+                    },
+                  }}
+                >
+                  <BsGithub className="w-8 h-8" />
+                </motion.a>
+                <motion.a
+                  href="https://github.com/uyenlai"
+                  rel="noopener noreferrer"
+                  className="rounded-lg bg-slate-200 p-2 px-6 text-lg font-semibold text-slate-800"
+                  whileHover={{
+                    scale: 1.05,
+                    transition: {
+                      duration: 0.3,
+                    },
+                  }}
+                >
+                  Visit project
+                </motion.a>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex-col items-center justify-center rounded-3xl rounded-br-2xl border border-solid border-l-cyan-900 shadow-[-2px_-2px_30px_2px_rgba(255,255,255,0.5),_20px_20px_30px_4px_rgba(45,78,255,0.15)] overflow-hidden">
+            <a
+              href="https://github.com/uyenlai"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer overflow-hidden rounded-lg"
+            >
+              <motion.img
+                src={projectImg}
+                alt="project 1"
+                className="h-auto w-full rounded-[2rem] p-5"
+                whileHover={{
+                  opacity: 0.9,
+                  scale: 1.01,
+                  transition: {
+                    duration: 0.5,
+                  },
+                }}
+              />
+            </a>
+
+            <div className="flex flex-col items-start justify-between p-8">
+              <h3 className="text-xl font-medium text-teal-500">
+                Featured Project
+              </h3>
+              <h2 className="my-2 w-full text-left text-4xl font-bold text-slate-200">
+                Project 1
+              </h2>
+              <p className="my-2 rounded-md font-medium text-slate-300">
+                A feature-rich Crypto Screener App using React, Tailwind CSS,
+                Context API, React Router and Recharts. It shows detail
+                regarding almost all the cryptocurrency. You can easily convert
+                the price in your local currency.
+              </p>
+              <div className="flex gap-4 items-center mt-2">
+                <motion.a
+                  href="https://github.com/uyenlai"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white"
+                  whileHover={{
+                    opacity: 0.8,
+                    scale: 1.2,
+                    transition: {
+                      duration: 0.3,
+                    },
+                  }}
+                >
+                  <BsGithub className="w-8 h-8" />
+                </motion.a>
+                <motion.a
+                  href="https://github.com/uyenlai"
+                  rel="noopener noreferrer"
+                  className="rounded-lg bg-slate-200 p-2 px-6 text-lg font-semibold text-slate-800"
+                  whileHover={{
+                    scale: 1.05,
+                    transition: {
+                      duration: 0.3,
+                    },
+                  }}
+                >
+                  Visit project
+                </motion.a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
