a17c84708ffdf4f3e773cfebe5f36a8f9abf77af
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-12 03:17:49 +0200

    skills page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
       fontFamily: {
         mont: ['"Montserrat"', "sans-serif"],
       },
+      backgroundImage: {
+        radialBg:
+          "repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px, #f5f5f5 100px)",
+      },
     },
   },
   plugins: [],
