b8cc0fa2b76af3514eb7b08fc23001325afc39b0
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-20 01:43:18 +0200

    update new resume

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import Button from "../components/Button";
 import { Link } from "react-scroll";
-import resume from "/UyenLai - Resume.pdf";
+import resume from "/Uyen Lai - Resume.pdf";
 
 export default function HomePage() {
   const textVariants = {
