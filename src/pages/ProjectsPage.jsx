b2de38fc92a8b2beb7574395b1822b86749895ff
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-23 05:44:34 +0200

    modify the events page project

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -35,14 +35,14 @@ export default function ProjectsPage() {
             href="https://github.com/uyenlai/tabs-navigation"
           />
         </div>
-        <div>
+        {/* <div>
           <Project
             title="Events Page"
             src={eventsPage}
             para="Experience seamless event management on my simple Events Page. Users can effortlessly create a new account or log in, unlocking exclusive privileges. Authenticated users gain the ability to modify or delete events, ensuring a personalized experience. The streamlined design, with a focus on authentication, allows users to enjoy the platform's functionality while backend data retrieval ensures up-to-date and dynamic content from the online source."
             href="https://github.com/uyenlai/events-page"
           />
-        </div>
+        </div> */}
         <div>
           <Project
             title="Guess The Number"
